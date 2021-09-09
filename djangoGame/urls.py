8f3cc195c9243ddd32e409d6be9fc94bc88ed116
Toshio <ctoshioyoshikawa@gmail.com>
2021-09-09 15:07:49 -0500

    add log in and log out

--- djangoGame/urls.py
+++ djangoGame/urls.py
@@ -19,11 +19,15 @@ from django.urls import path
 from account.views import (
     home_screen_view,
     registration_view,
+    logout_view,
+    login_view
 )
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
-    path('register/', registration_view, name="register")
+    path('register/', registration_view, name="register"),
+    path('logout/', logout_view, name="logout"),
+    path('login/', login_view, name="login"),
 ]
